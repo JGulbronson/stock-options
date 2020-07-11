e158ccbe388a1341ac871ac93dc620c63d4fb5de
Jeff Gulbronson <jgulbronson@gmail.com>
2020-07-11 10:59:47 -0400

    Use tsx files and use Material UI

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button, Container } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <Container fixed>
+      <Button variant="contained" color="primary">
+        Click me
+      </Button>
+    </Container>
+  );
+};
+
+export default App;
